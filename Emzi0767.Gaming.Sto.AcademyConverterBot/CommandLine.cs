b31300d49346aba6a4c59af497ac00d2fd9da0ea
Emzi0767 <matiz100@windowslive.com>
2016-10-30 17:58:40 +0100

    Initial commit

---
+++ Emzi0767.Gaming.Sto.AcademyConverterBot/CommandLine.cs
@@ -0,0 +1,11 @@
+﻿namespace Emzi0767.Gaming.Sto.AcademyConverterBot
+{
+    internal class CommandLine
+    {
+        [CommandLineParameter("single")]
+        public string SingleId { get; set; }
+
+        [CommandLineParameter("target")]
+        public string Target { get; set; }
+    }
+}
